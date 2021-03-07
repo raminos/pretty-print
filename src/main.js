3066668ead34df0971ebed9e6be054139ff3b65f
Ramin Azhdari <47226721+raminos@users.noreply.github.com>
2021-03-07 01:05:33 +0100

    clean up functions

--- src/main.js
+++ src/main.js
@@ -1,35 +1,35 @@
-// Are you using `console.log('variable: ', variable)` a lot?
-// Use this variable instead and save yourself the typing.
-// Usage: print({ foo, bar })
-// See the example below
-export default function print(elements) {
-  if (!(elements instanceof Object)) {
-    throw TypeError(
-      `Expected argument to be of type 'object' but got '${typeof elements}' instead`,
-    );
-  }
+const errorMessage = (elementsType) =>
+  `Expected argument to be of type 'object' but got '${elementsType}' instead.`;
 
-  Object.entries(elements).forEach(([name, value]) => {
-    console.log(name + ":", value);
-  });
+function forEachObject(obj, callback) {
+  Object.entries(obj).forEach(([key, value]) => callback(key, value));
 }
 
-// The prettyPrint function additionally displays collections in a table format.
-export function prettyPrint(elements) {
-  if (!(elements instanceof Object)) {
-    throw TypeError(
-      `Expected argument to be of type 'object' but got '${typeof elements}' instead`,
-    );
-  }
+function log(name, value) {
+  console.log(name + ":", value);
+}
 
-  Object.entries(elements).forEach(([name, value]) => {
-    if (Array.isArray(value) || value instanceof Object) {
-      console.log(name + ":");
-      console.table(value);
-    } else {
-      console.log(name + ":", value);
-    }
-  });
+function tableLog(name, values) {
+  console.log(name + ":");
+  console.table(values);
+}
+
+export default function print(elements) {
+  if (!(elements instanceof Object))
+    throw TypeError(errorMessage(typeof elements));
+
+  forEachObject(elements, (name, value) => log(name, value));
+}
+
+export function prettyPrint(elements) {
+  if (!(elements instanceof Object))
+    throw TypeError(errorMessage(typeof elements));
+
+  forEachObject(elements, (name, value) =>
+    Array.isArray(value) || value instanceof Object
+      ? tableLog(name, value)
+      : log(name, value)
+  );
 }
 
 const num = 10;
@@ -39,36 +39,4 @@ const people = [person, { name: "Elise", age: 26 }];
 const nums = [1, 2, 3, 4, 5];
 
 print({ num, word, person, people, nums });
-// num: 10
-// word: I'm a string
-// person: { name: 'Ramin', age: 28 }
-// people: [ { name: 'Ramin', age: 28 }, { name: 'Elise', age: 26 } ]
-// nums: [ 1, 2, 3, 4, 5 ]
-
 prettyPrint({ num, word, person, people, nums });
-// num: 10
-// word: I'm a string
-// person:
-// ┌───────┬─────────┐
-// │ (idx) │ Values  │
-// ├───────┼─────────┤
-// │ name  │ "Ramin" │
-// │  age  │   28    │
-// └───────┴─────────┘
-// people:
-// ┌───────┬─────────┬─────┐
-// │ (idx) │  name   │ age │
-// ├───────┼─────────┼─────┤
-// │   0   │ "Ramin" │ 28  │
-// │   1   │ "Elise" │ 26  │
-// └───────┴─────────┴─────┘
-// nums:
-// ┌───────┬────────┐
-// │ (idx) │ Values │
-// ├───────┼────────┤
-// │   0   │   1    │
-// │   1   │   2    │
-// │   2   │   3    │
-// │   3   │   4    │
-// │   4   │   5    │
-// └───────┴────────┘
